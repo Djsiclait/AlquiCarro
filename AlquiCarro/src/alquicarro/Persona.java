48d04f2c795ea6d424d6fad4d34e28af00d480b7
AlfredoGru <Alfredogrullon1@gmail.com>
2019-10-28 16:29:15 -0400

    Crear Clases Persona, Cliente, Agente

---
+++ AlquiCarro/src/alquicarro/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alquicarro;
+
+/**
+ *
+ * @author Alfredo G.
+ */
+public class Persona {
+    
+    private String Nombre = "Alejandro Almonte";
+    private String Direccion = "La Terraza";
+    private String Telefono = "8091239874";
+    
+    public void Saludo() {
+        System.out.println("Hola, Soy el agente " +Nombre);
+    }
+    
+}
