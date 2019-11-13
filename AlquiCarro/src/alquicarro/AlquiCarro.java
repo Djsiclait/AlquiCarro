c5a3f5f2848f3fccaccbf12f4b40b5368d758c51
Djidjelly Siclait <djidjellypas92@gmail.com>
2019-11-14 00:08:55 +0100

    Limpiar codigo

--- AlquiCarro/src/alquicarro/AlquiCarro.java
+++ AlquiCarro/src/alquicarro/AlquiCarro.java
@@ -16,97 +16,93 @@ public class AlquiCarro {
     static ArrayList<Carro> listaCarro = new ArrayList<>();
     
     public static void main(String[] args) {
-        
-     // Lista de Carros
-    listaCarro.add(new Carro("Toyota","Camri",2014,750000,0,"6 Personas",1));
-    listaCarro.add(new Carro("Toyota","4Runner",2020,3000000,0,"6 Personas",2));
-    listaCarro.add(new Carro("Lexus","LX570",2020,8000000,0,"6 personas",3));
-    listaCarro.add(new Carro("Toyota","Land Cruirse PradoVXL",2020,4500000,0,"6 Personas",4));
-    listaCarro.add(new Carro("Hyundai","Sante fe Sport",2020,2500000,0,"6 Personas",5));
-    listaCarro.add(new Carro("Isuzu","DMAX",2020,2650000,0,"6 Personas",6));
-    listaCarro.add(new Carro("Kia","Sorento",2020,2000000,0,"6 Personas",7));
-    listaCarro.add(new Carro("MercedesBenz","Clase Gle450",2020,4000000,0,"6 Personas",8));
-    listaCarro.add(new Carro("BMW","Serie X5",2020,5500000,0,"6 Personas",9));
-    listaCarro.add(new Carro("Nissan","Frontier",2020,2000000,0,"6 Personas",10));
-        
-       String respuesta; // v6ariable para guardar entradsa del usuario
+        // Lista de Carros
+        listaCarro.add(new Carro("Toyota","Camri",2014,750000,0,"6 Personas",1));
+        listaCarro.add(new Carro("Toyota","4Runner",2020,3000000,0,"6 Personas",2));
+        listaCarro.add(new Carro("Lexus","LX570",2020,8000000,0,"6 personas",3));
+        listaCarro.add(new Carro("Toyota","Land Cruirse PradoVXL",2020,4500000,0,"6 Personas",4));
+        listaCarro.add(new Carro("Hyundai","Sante fe Sport",2020,2500000,0,"6 Personas",5));
+        listaCarro.add(new Carro("Isuzu","DMAX",2020,2650000,0,"6 Personas",6));
+        listaCarro.add(new Carro("Kia","Sorento",2020,2000000,0,"6 Personas",7));
+        listaCarro.add(new Carro("MercedesBenz","Clase Gle450",2020,4000000,0,"6 Personas",8));
+        listaCarro.add(new Carro("BMW","Serie X5",2020,5500000,0,"6 Personas",9));
+        listaCarro.add(new Carro("Nissan","Frontier",2020,2000000,0,"6 Personas",10));
+
+        String respuesta; // v6ariable para guardar entradsa del usuario
+        Scanner entrada = new Scanner(System.in); // instancia del lector de entrada de usuario
+
+        boolean error = false;
        
-        
-       Scanner entrada = new Scanner(System.in); // instancia del lector de entrada de usuario
-       
-       boolean error = false;
-       
-       do{
-       // Mensaje de Bienvenida
-       System.out.println("Bienvenido a AlquiCarro: tu solición para alquiler de carros\n");
-       
-       // Presentando el menú al usuario
-       System.out.println("Favor elegir la operación que deseas realizar:");
-       
-       System.out.println("Registración");
-       System.out.println("\t1. Registrar Nuevo Cliente");
-       System.out.println("\t2. Registrar Nuevo Carro");
-       System.out.println("\t3. Registrar Nuevo Agente");
-       System.out.println("\t4. Registrar Nuevo Alquiler");
-       
-       System.out.println("Consultas");
-       System.out.println("\t5. Ver Registro de Clientes");
-       System.out.println("\t6. Ver Registro de Carros");
-       System.out.println("\t7. Registrar Nuevo Agente");
-       System.out.println("\t8. Ver Registro de Alquileres");
-       
-       System.out.println("Cobros");
-       System.out.println("\t9. Cobrar Alquiler");
-       
-       // Capturar respuesta del usuario
-       System.out.println("Elegir el codigo de la operación o presionar 0 para cerrar la aplicación.");
-           if (error) {
-               System.out.println("Favor introducir una opcion válida");
-               error = false;
-           }
-       System.out.print("Respuesta : ");
-       respuesta = entrada.nextLine(); // capturando la respuesta del usuarios
-       
-       // Cerebro del la aplicación
-       // Este parte permite el programa interpretar la entrada el usuario y ejecutar las funcionalidades elegidas
-       switch(respuesta){
-           case "0":
-               break;
-           case "1":
-               RegistrarCliente();
-               break;
-           case "2":
-               RegistrarCarro();
-               break;
-           case "3":
-               RegistrarAgente();
-               break;
-           case "4":
-               RealizarAlquiler();
-               break;
-           case "5":
-               ListarClientes();
-               break;
-           case "6":
-               ListarCarro();
-               break;
-           case "7":
-               ListarAgentes();
-               break;
-           case "8":
-               ListarAlquileres();
-               break;
-           case "9":
-               CobrarAlquiler();
-               break;
-           default:
-               error = true;
-               break;
-       }
-       
-    }   while(!respuesta.equals("0")); // mantener la applicacion corriendo hasta que el usuario elige cerrarlo con 0
-       
-        System.out.println("\n\n\t¡Gracias por preferir AlquiCarro! Hasta la proxima...");
+        do{
+            // Mensaje de Bienvenida
+            System.out.println("Bienvenido a AlquiCarro: tu solición para alquiler de carros\n");
+
+            // Presentando el menú al usuario
+            System.out.println("Favor elegir la operación que deseas realizar:");
+
+            System.out.println("Registración");
+            System.out.println("\t1. Registrar Nuevo Cliente");
+            System.out.println("\t2. Registrar Nuevo Carro");
+            System.out.println("\t3. Registrar Nuevo Agente");
+            System.out.println("\t4. Registrar Nuevo Alquiler");
+
+            System.out.println("Consultas");
+            System.out.println("\t5. Ver Registro de Clientes");
+            System.out.println("\t6. Ver Registro de Carros");
+            System.out.println("\t7. Registrar Nuevo Agente");
+            System.out.println("\t8. Ver Registro de Alquileres");
+
+            System.out.println("Cobros");
+            System.out.println("\t9. Cobrar Alquiler");
+
+            // Capturar respuesta del usuario
+            System.out.println("Elegir el codigo de la operación o presionar 0 para cerrar la aplicación.");
+            if (error) {
+                System.out.println("Favor introducir una opcion válida");
+                error = false;
+            }
+            
+            System.out.print("Respuesta : ");
+            respuesta = entrada.nextLine(); // capturando la respuesta del usuarios
+
+            // Cerebro del la aplicación
+            // Este parte permite el programa interpretar la entrada el usuario y ejecutar las funcionalidades elegidas
+            switch(respuesta){
+                case "0":
+                    System.out.println("\n\n\t¡Gracias por preferir AlquiCarro! Hasta la proxima...");
+                    break;
+                case "1":
+                    RegistrarCliente();
+                    break;
+                case "2":
+                    RegistrarCarro();
+                    break;
+                case "3":
+                    RegistrarAgente();
+                    break;
+                case "4":
+                    RealizarAlquiler();
+                    break;
+                case "5":
+                    ListarClientes();
+                    break;
+                case "6":
+                    ListarCarros();
+                    break;
+                case "7":
+                    ListarAgentes();
+                    break;
+                case "8":
+                    ListarAlquileres();
+                    break;
+                case "9":
+                    CobrarAlquiler();
+                    break;
+                default:
+                    error = true;
+                    break;
+            }
+        }while(!respuesta.equals("0")); // mantener la applicacion corriendo hasta que el usuario elige cerrarlo con 0
     }
     
     public static void RegistrarCliente(){
@@ -114,40 +110,37 @@ public class AlquiCarro {
     }
     
     public static void RegistrarCarro(){
-     // Capturar la informacion del usuario
-    Scanner teclado= new Scanner(System.in);
-        
-    System.out.println("Marca: ");
-    String Marca= teclado.nextLine();    // Capturando la informacion del usuario.
-    System.out.println("Modelo: ");
-    String Modelo= teclado.nextLine();   // Capturando la informacion del usuario.
-    System.out.println("Ano: ");
-    int Ano = teclado.nextInt();         // Capturando la informacion del usuario.
-    System.out.println("Precio: ");
-    int Precio = teclado.nextInt();      // Capturando la informacion del usuario.
-    System.out.println("Deposito: ");
-   int Deposito = teclado.nextInt();    // Capturando la informacion del usuario.
-    System.out.println("IDCarro: ");
-    int IDCarro = teclado.nextInt();     // Capturando la informacion del usuario.
-    System.out.println("Capacidad: ");
-    String Capacidad = teclado.nextLine();
-    
-    listaCarro.add(new Carro(Marca,Modelo,Ano,Precio,Deposito,Capacidad,IDCarro));
-    
-    System.out.println(" \t 'Informacion Capturada' ");
-    
+        // Capturar la informacion del usuario
+       Scanner teclado= new Scanner(System.in);
+
+       System.out.println("Marca: ");
+       String Marca= teclado.nextLine();    // Capturando la marca del carro.
+       
+       System.out.println("Modelo: ");
+       String Modelo= teclado.nextLine();   // Capturando el modelo del carro.
+       
+       System.out.println("Año: ");
+       int Ano = teclado.nextInt();         // Capturando el año del carro.
+       
+       System.out.println("Precio: ");
+       int Precio = teclado.nextInt();      // Capturando el precio del carro.
+       
+       System.out.println("Deposito: ");
+       int Deposito = teclado.nextInt();    // Capturando el deposito del carro.
+       
+       System.out.println("IDCarro: ");
+       int IDCarro = teclado.nextInt();     // Capturando el id del carro
+       
+       System.out.println("Capacidad: ");
+       String Capacidad = teclado.nextLine();   // Capturando la capacidad del carro
+
+       listaCarro.add(new Carro(Marca,Modelo,Ano,Precio,Deposito,Capacidad,IDCarro));
+
+       System.out.println(" \t 'Informacion Capturada' ");
+       
+       ListarCarros();
+       
    }
-      public static String imprimirLista(ArrayList<Carro> lista){
-        String result="";
-      
-        for (int i = 0; i < lista.size(); i++) {
-            result += lista.get(i).toString();
-            
-        }
-        
-        return result;
-   
-    }
     
     public static void RegistrarAgente(){
         
@@ -165,13 +158,10 @@ public class AlquiCarro {
         
     }
     
-    public static void ListarCarro(){
-        
-     for (int   i= 0;i <listaCarro.size();  i++) {
-       System.out.println(listaCarro.get(i));
-      
-        }
-       
+    public static void ListarCarros(){
+        listaCarro.stream().forEach((carro) -> {
+            System.out.println(carro);
+        });      
     }
     
     public static void ListarAgentes(){
@@ -179,7 +169,6 @@ public class AlquiCarro {
     }
     
     public static void CobrarAlquiler(){
-        
-       System.out.println("Cobros");
+        System.out.println("Cobros");
     }
 }
